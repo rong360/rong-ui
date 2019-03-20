1d83460efd887793bada703218d12bd900eea2af
jay200212003 <397602528@qq.com>
2019-03-20 19:59:09 +0800

    rVcode bug fix

--- src/components/rVcode/index.js
+++ src/components/rVcode/index.js
@@ -1,9 +1,9 @@
-import rVcode from "./rvcode"
-import toast from "../toast"
+import rVcode from './rVcode'
+import toast from '../toast'
 
-rVcode.install = function(Vue){
-	Vue.component(rVcode.name, rVcode);
-	Vue.use(toast);
+rVcode.install = function (Vue) {
+  Vue.component(rVcode.name, rVcode)
+  Vue.use(toast)
 }
 
-export default rVcode;
+export default rVcode
