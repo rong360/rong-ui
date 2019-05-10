0b4df25c9e84aca69c3f4b7ba3556bf17000de70
jay200212003 <397602528@qq.com>
2019-05-10 20:44:49 +0800

    修复rInput不能清空问题

--- src/components/rInput/rInput.vue
+++ src/components/rInput/rInput.vue
@@ -33,158 +33,160 @@
 	</div>
 </template>
 <script>
-	import rIcon from "../rIcon/rIcon"
+	import rIcon from '../rIcon/rIcon'
 	export default{
-		name: "rInput",
-		components:{
-			rIcon
-		},
-		props:{
-			attrs: Object
-		},
-		data(){
-			return {
-				currentValue: this.attrs.value || "",
-				showKb: false //控制光标
-			}
-		},
-		computed:{
-			conf(){
-				let defaultConfig = {
-						title: "表单名称",
-						lr: "right",
-						name: '',
-						value: "",
-						placeholder: "请输入",
-						lblWidth: {},
-						maxlength: -1,
-						disabled: false,
-						readonly: false,
-						unit: '',
-						inputType: 'text',
-						//显示底部border
-						showBottomBorder: true,
-						//显示input后面icon
-						showInputIcon: true,
-						//input后面icon的类型
-						inputIconType: "close-circled",
-						//显示label后面的icon
-						showLabelIcon: false,
-						//label后面icon的类型
-						labelIconType: 'info',
-						needVerify: true,
-						verify: function(){ return true },
-						needVerifyToast: true,
-						showLabel: true
-					};
-				return this.extendObj(defaultConfig,this.attrs);
-			},
-			lblStyle(){
-				return {"width": this.conf.lblWidth}
-			},
-			showInputIcon(){
-				if(this.conf.inputIconType == "close-circled"){
-					if(this.conf.showInputIcon && this.currentValue && !this.conf.readonly && this.showKb){
-						return true;
-					}else{
-						return false;
-					}
-				}else{
-					return this.conf.showInputIcon;
-				}
-			},
-			iClsName(){
-				return this.conf.lr=="right" ? "ta-right" : "ta-left";
-			}
-		},
-		watch:{
-			conf(){
-				this.currentValue = this.conf.value;
-			}
-		},
-		methods:{
-			onEnter(e){
-				this.$emit("onenter",e, this);
-			},
-			onFocus(e){
-				this.$emit("onfocus",e, this);
-				this.showKb = true
-			},
-			onBlur(e){
-				this.$emit("onblur",e, this);
-				this.showKb = false
-			},
-			onInput(e){
-				this.currentValue = e.target.value;	
-				this.$emit("oninput",e, this);
-
-			},
-			onChange(e){
-				this.$emit("onchange",e, this);
-			},
-			onclickLabelIcon(e){
-				this.$emit("onclickLabelIcon", this);
-			},
-		    onclickInputIcon(e){
-		    	if(this.conf.inputIconType == 'close-circled'){
-		    		this.$el.querySelector("input").focus();
-		    		this.currentValue = "";
-		    		this.$emit("onclear", this);
-		    	}else{
-		    		this.$emit("onclickInputIcon", this);
-		    	}
+	  name: 'rInput',
+	  components: {
+	    rIcon
+  },
+	  props: {
+	    attrs: Object
+  },
+	  data () {
+    return {
+	      currentValue: this.attrs.value || '',
+	      showKb: false // 控制光标
+    }
+  },
+	  computed: {
+	    conf () {
+	      let defaultConfig = {
+	        title: '表单名称',
+	        lr: 'right',
+	        name: '',
+	        value: '',
+	        placeholder: '请输入',
+	        lblWidth: {},
+	        maxlength: -1,
+	        disabled: false,
+	        readonly: false,
+	        unit: '',
+	        inputType: 'text',
+						// 显示底部border
+	        showBottomBorder: true,
+						// 显示input后面icon
+	        showInputIcon: true,
+						// input后面icon的类型
+	        inputIconType: 'close-circled',
+						// 显示label后面的icon
+	        showLabelIcon: false,
+						// label后面icon的类型
+	        labelIconType: 'info',
+	        needVerify: true,
+	        verify: function () { return true },
+	        needVerifyToast: true,
+	        showLabel: true
+      }
+	      return this.extendObj(defaultConfig, this.attrs)
+	    },
+	    lblStyle () {
+	      return {'width': this.conf.lblWidth}
+    },
+	    showInputIcon () {
+	      if (this.conf.inputIconType == 'close-circled') {
+	        if (this.conf.showInputIcon && this.currentValue && !this.conf.readonly && this.showKb) {
+	          return true
+	        } else {
+	          return false
+	        }
+      } else {
+	        return this.conf.showInputIcon
+	      }
+    },
+	    iClsName () {
+	      return this.conf.lr == 'right' ? 'ta-right' : 'ta-left'
+    }
+  },
+	  watch: {
+	    conf () {
+	      this.currentValue = this.conf.value
+	    }
+  },
+	  methods: {
+	    onEnter (e) {
+	      this.$emit('onenter', e, this)
+	    },
+	    onFocus (e) {
+	      this.$emit('onfocus', e, this)
+	      this.showKb = true
+    },
+	    onBlur (e) {
+	      let self = this
+	      this.$emit('onblur', e, this)
+	      setTimeout(function () {
+	        self.showKb = false
+      }, 50)
+    },
+	    onInput (e) {
+	      this.currentValue = e.target.value
+	      this.$emit('oninput', e, this)
+	    },
+	    onChange (e) {
+	      this.$emit('onchange', e, this)
+	    },
+	    onclickLabelIcon (e) {
+	      this.$emit('onclickLabelIcon', this)
+	    },
+		    onclickInputIcon (e) {
+		    	if (this.conf.inputIconType == 'close-circled') {
+		    		this.$el.querySelector('input').focus()
+	    		this.currentValue = ''
+	    		this.$emit('onclear', this)
+	    	} else {
+		    		this.$emit('onclickInputIcon', this)
+	    	}
 		    },
-			verify(){
-				if(this.conf.needVerify){
-					if(this.defaultVerify()){
-						var res = this.conf.verify.apply(this,[this.currentValue]);
-						if(typeof res == "undefined"){
-							console.warn(this.conf.title + "组件 自定义校验函数verify中if else 必须有true和false返回值, 如：if(true){ return true } else{ return false }, 否则会忽略自定义校验函数，改为基本的非空校验!");
-							return this.defaultVerify();
-						}
-						return res;
-					}else{
-						return false;
-					}
-				}else{
-					return true;
-				}				
-			},
-			defaultVerify(){
-				if(!this.currentValue){
-					this.conf.needVerifyToast&&this.$toast && this.$toast({
-						propsData: {
-							message:this.conf.title + "不能为空！",
-							type:"negetive"
-						}
-					});
-					return false;
-				}
-				return true;				
-			},
-			getSerializeArray(){
-				let obj = {};
-				obj[this.conf.name] = this.currentValue;
-				return obj;
-			},
-			getSerializeObject(){
-				return {
-					"name": this.conf.name,
-					"value": this.currentValue
-				}
-			},
-			getSerialize(){
-				return this.conf.name+"="+this.currentValue;
-			},
-			extendObj(obj1,obj2){
-		        for(var key in obj2){
-		            obj1[key] = obj2[key];
-		        }
-		        obj1.name = obj1.var_name || obj1.urlkey || obj1.name; //业务中的组件name取值
-		        if(!obj1.name) console.warn("组件未传入name属性！title="+obj1.title);
-		        return obj1;
-		    }
-		}
+	    verify () {
+	      if (this.conf.needVerify) {
+	        if (this.defaultVerify()) {
+	          var res = this.conf.verify.apply(this, [this.currentValue])
+	          if (typeof res === 'undefined') {
+	            console.warn(this.conf.title + '组件 自定义校验函数verify中if else 必须有true和false返回值, 如：if(true){ return true } else{ return false }, 否则会忽略自定义校验函数，改为基本的非空校验!')
+	            return this.defaultVerify()
+          }
+	          return res
+	        } else {
+	          return false
+	        }
+      } else {
+	        return true
+	      }
+    },
+	    defaultVerify () {
+	      if (!this.currentValue) {
+	        this.conf.needVerifyToast && this.$toast && this.$toast({
+	          propsData: {
+	            message: this.conf.title + '不能为空！',
+	            type: 'negetive'
+          }
+        })
+	        return false
+	      }
+	      return true
+	    },
+	    getSerializeArray () {
+	      let obj = {}
+	      obj[this.conf.name] = this.currentValue
+	      return obj
+	    },
+	    getSerializeObject () {
+      return {
+	        'name': this.conf.name,
+	        'value': this.currentValue
+      }
+    },
+	    getSerialize () {
+	      return this.conf.name + '=' + this.currentValue
+	    },
+	    extendObj (obj1, obj2) {
+		        for (var key in obj2) {
+		            obj1[key] = obj2[key]
+	        }
+		        obj1.name = obj1.var_name || obj1.urlkey || obj1.name // 业务中的组件name取值
+	      if (!obj1.name) console.warn('组件未传入name属性！title=' + obj1.title)
+	      return obj1
+	    }
+  }
 	}
 </script>
 <style lang="scss">
